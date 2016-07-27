316838199b9686c3ec75f1db52f85ae4b81303e2
Sampson <dujianwxfdt@live.cn>
2016-07-27 09:02:14 +0800

    add get max num

--- src/base.js
+++ src/base.js
@@ -6,6 +6,9 @@ import Log from './log.js'
 
 var refLog = null;
 
+const sleep =(ms)=> new Promise(resolve=>setTimeout(resolve,ms))
+
+
 const add = ()=>{
   Rx.Observable.range(1,50).reduce((acc,v)=>{
     return acc + v;
@@ -24,6 +27,22 @@ const add2 = ()=>{
   })
 }
 
+const asyncFunc = ()=>{
+  Rx.Observable.startAsync(()=>sleep(5000)).subscribe(()=>{
+    refLog.info('log',"Async Operation succeeded!")
+
+  })
+}
+
+const getMaxnum = ()=>{
+  Rx.Observable.from(['0','7','-','99','-1']).map((x)=>parseInt(x))
+  .filter(x => x )
+  .reduce((prev,x)=>prev>x?prev:x)
+  .subscribe((x)=>{
+    refLog.info('log',`max num of this array is ${x}`)
+  });
+}
+
 class Base extends React.Component{
 
 
@@ -34,6 +53,8 @@ class Base extends React.Component{
         <h3>需求场景：一些比较基础的用法</h3>
         <input type="button" value="累加器(1+50)" onClick={add} />
         <input type="button" value="累加器(1^2+50^2)" onClick={add2} />
+        <input type="button" value="处理耗时操作" onClick={asyncFunc} />
+        <input type="button" value="取最大值['0','7','-','99','-1']" onClick={getMaxnum} />
 
         <input type="button" value="Clear Log" onClick={()=>{refLog.clear();this.setState({show:false})}}/>
 
